fd863bc13342690d3c2227ccb82aadba90bb986a
Victor Ho <victort509dm@gmail.com>
2023-04-30 17:24:20 +0800

    add firebase

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,24 @@
+
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
+import 'antd/dist/reset.css';
 import './App.css'
-import 'antd/dist/reset.css'
 import Home from './pages/Home'
-
+import {
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query'
+const queryClient = new QueryClient();
+// import { feedImages } from "./api"
+// feedImages();
 
 function App() {
-
-  const [count, setCount] = useState(0)
-
-    return(
-      <div>
-        <Home/>
-      </div>
-      
-    );
   
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Home />
+    </QueryClientProvider>
+  );
 }
 
-export default App
+export default App;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,15 +2,33 @@
 import Header from "../components/Header";
 import ProductList from "../components/ProductList";
 import Footer from "../components/Footer";
+import { useImages } from "../react-query";
 
 function Home() {
+
+    const { data, isLoading } = useImages();
+    const images = data || [];
+
     return(
         <div className="container main-layout">
-            <Header className = "layout-header"/>
-            <ProductList className = "layout-content"/>
+            <Header 
+                className="layout-header"
+                titie="photoweb"
+                slogan="Victor's Web"
+            />
+            <ProductList
+                className = "layout-content"
+                images={images}
+                isLoading={isLoading}
+
+            />
             <Footer className = "layout-footer"/>
         </div>
 
     );
 }
 export default Home;
+
+
+
+
